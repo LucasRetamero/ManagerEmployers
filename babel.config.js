3f906a1de66b949df59a2de903d5041ae2cd2e01
LucasRetamero <lucasretamerogimenes@gmail.com>
2024-12-06 20:52:29 -0300

    Put ORM and setting up configuration to run on emulator android

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
-  const plugins = [];
+  const plugins = [['@babel/plugin-proposal-decorators', { legacy: true }]];
 
   return {
     presets: ['babel-preset-expo'],
